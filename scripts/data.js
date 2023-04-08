9dc679f76bb6c2c7afca1ffd6155720867279ab6
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-08 18:46:37 +0300

    st

--- scripts/data.js
+++ scripts/data.js
@@ -2,7 +2,7 @@ const header = "Документы"
 
 const categories = [
   { "name": "Обязательные для всех", "documents": [{"name":"fsgs", "required": true, "comment": "Для всех"}, {"name":"fsgs", "required": true, "comment": "Для всех"}], "comment": "Для всех"},
-  { "name": "Обязательные для трудоустройства", "documents": [{"name":"fsgs", "required": true, "comment": "Для всех"}, {"name":"fsgs", "required": true, "comment": "Для всех"}], "comment": "Для всех"},
+  { "name": "Обязательные для трудоустройства", "documents": [{"name":"fsgs", "required": false, "comment": "Для всех"}, {"name":"fsgs", "required": true, "comment": "Для всех"}], "comment": "Для всех"},
   { "name": "Специальные", "documents": [{"name":"fsgs", "required": true, "comment": "Для всех"}, {"name":"fsgs", "required": true, "comment": "Для всех"}], "comment": "Для всех"}
 ]
 
--- scripts/main.js
+++ scripts/main.js
@@ -48,7 +48,10 @@ function ReservationsViewModel() {
 
     this.handleDragStart = function (data, event) {
 
-        event.target.style.opacity = '0.4';
+        console.log(event)
+
+        // event.target.toElement.style.opacity = '0.4';
+        // event.target.style.border= "3px solid red";
         const objectIndex = self.documents().findIndex(element => element === data)
 
         event.dataTransfer.setData('text/plain', objectIndex)
@@ -97,6 +100,155 @@ function ReservationsViewModel() {
         // self.documents.splice(fromIndex, 0, self.documents.splice(toIndex, 1)[0]);
         return false;
     }
+
+    let clone = null;
+    let rowWidth = 0;
+
+    this.dragClick = function(data, e) {
+        const row = e.target.parentElement.parentElement
+        clone = row.cloneNode(true)
+        rowWidth = e.currentTarget.parentElement.parentElement.offsetWidth
+
+        clone.style.position='absolute';
+        clone.style.left= e.clientX - rowWidth + 'px';
+        clone.style.top = e.clientY + 'px';
+        clone.style.width = rowWidth + "px"
+        // clone.style.offset= "0px, 3px rgba(0, 102, 255, 0.7)"
+        clone.style.boxShadow= "0px 3px 16px rgba(0, 102, 255, 0.7)"
+
+        clone.style.border= "1px solid #DFE4EF"
+
+
+        document.body.appendChild(clone);
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        window.onmousemove=handleMouseMove;
+        // window.addEventListener("onmouseup", handleMouseUp)
+        window.onmouseup=handleMouseUp;
+    }
+
+    function handleMouseMove(e){
+        // e.preventDefault();
+        // e.stopPropagation();
+        // get mouse position
+        startX=parseInt(e.clientX);
+        startY=parseInt(e.clientY);
+
+        clone.style.left= e.clientX - rowWidth + 'px';
+        clone.style.top = e.clientY + 'px';
+
+        console.log("mousemove handler",startX, startY)
+
+        // window.onmouseup = function (e) {handleMouseUp(e)}
+    }
+
+    function handleMouseUp(e){
+        console.log("mouse up")
+        window.onmousemove = ()=>{}
+
+    }
+
+    // window.onmousedown=(function(e){handleMouseDown(e);});
+
+
+    // function handleMouseDown(e){
+    //     // tell the browser we're handling this event
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    //     // // get mouse position
+    //     startX=parseInt(e.clientX);
+    //     startY=parseInt(e.clientY);
+    //     console.log(startX)
+    //     console.log(startY)
+    //     // // Is any div under the mouse?
+    //     // draggingIndex=undefined;
+    //     // for(var i=0;i<divs.length;i++){
+    //     //     var d=divs[i];
+    //     //     var x=parseInt(d.div.style.left);
+    //     //     var y=parseInt(d.div.style.top);
+    //     //     if(startX>x && startX<x+d.w && startY>y && startY<y+d.h){
+    //     //         draggingIndex=i;
+    //     //         isDown=true;
+    //     //         break;
+    //     //     }
+    //     // }
+    // }
+
+
+
+
+
+//     // add some divs dynamically
+//     addDiv(50,50,100,75,'blue','batch1');
+//     addDiv(250,50,50,38,'green','batch1');
+//
+// // listen to mouse events
+//     window.onmousedown=(function(e){handleMouseDown(e);});
+//     window.onmousemove=(function(e){handleMouseMove(e);});
+//     window.onmouseup=(function(e){handleMouseUp(e);});
+//
+//
+//     function addDiv(x,y,w,h,bk,classname){
+//         var div=document.createElement('div');
+//         div.style.width=w+'px';
+//         div.style.height=h+'px';
+//         div.className=classname;
+//         div.style.position='absolute';
+//         div.style.left=x+'px';
+//         div.style.top=y+'px';
+//         div.style.background=bk;
+//         divs.push({div:div,w:w,h:h});
+//         document.body.appendChild(div);
+//     }
+//
+//
+//     function handleMouseDown(e){
+//         // tell the browser we're handling this event
+//         e.preventDefault();
+//         e.stopPropagation();
+//         // get mouse position
+//         startX=parseInt(e.clientX);
+//         startY=parseInt(e.clientY);
+//         // Is any div under the mouse?
+//         draggingIndex=undefined;
+//         for(var i=0;i<divs.length;i++){
+//             var d=divs[i];
+//             var x=parseInt(d.div.style.left);
+//             var y=parseInt(d.div.style.top);
+//             if(startX>x && startX<x+d.w && startY>y && startY<y+d.h){
+//                 draggingIndex=i;
+//                 isDown=true;
+//                 break;
+//             }
+//         }
+//     }
+//
+//     function handleMouseUp(e){
+//         // tell the browser we're handling this event
+//         e.preventDefault();
+//         e.stopPropagation();
+//         isDown=false;
+//     }
+//
+//     function handleMouseMove(e){
+//         if(!isDown){return;}
+//         // tell the browser we're handling this event
+//         e.preventDefault();
+//         e.stopPropagation();
+//         // get mouse position
+//         mouseX=parseInt(e.clientX);
+//         mouseY=parseInt(e.clientY);
+//         // move the dragging div by the distance the mouse has moved
+//         var dragging=divs[draggingIndex].div;
+//         var dx=mouseX-startX;
+//         var dy=mouseY-startY;
+//         startX=mouseX;
+//         startY=mouseY;
+//         dragging.style.left=(parseInt(dragging.style.left)+dx)+'px';
+//         dragging.style.top=(parseInt(dragging.style.top)+dy)+'px';
+//     }
 }
 
 ko.applyBindings(new ReservationsViewModel());
